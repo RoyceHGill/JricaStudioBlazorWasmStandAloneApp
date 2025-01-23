cc57c611ef28b4d0e0a4e0d1af8e30ab96ae4c5e
RoyzRonixx <124342183+RoyceHGill@users.noreply.github.com>
2025-01-23 12:59:25 +1000

    style

--- Pages/Appointments/BookingPageBase.cs
+++ Pages/Appointments/BookingPageBase.cs
@@ -44,7 +44,7 @@ namespace JricaStudioApp.Pages.Appointments
             get { return startTime; }
             set
             {
-                if (value != null)
+                if ( value != null )
                 {
                     this.EndTime = value + Duration;
 
@@ -55,13 +55,13 @@ namespace JricaStudioApp.Pages.Appointments
 
         protected async override Task OnInitializedAsync()
         {
-            Appointment = await AppointmentService.GetAppointment(Id);
+            Appointment = await AppointmentService.GetAppointment( Id );
 
             Duration = CalculateDuration();
             StartTime = Appointment.StartTime;
-            UnavailaleDatesDtos = await SchedulingService.GetUnavailableDates(ScheduleRange + 1, Duration);
+            UnavailaleDatesDtos = await SchedulingService.GetUnavailableDates( ScheduleRange + 1, Duration );
 
-            foreach (var item in UnavailaleDatesDtos)
+            foreach ( var item in UnavailaleDatesDtos )
             {
                 item.UnavailableDate = item.UnavailableDate.ToLocalTime();
             }
@@ -72,39 +72,39 @@ namespace JricaStudioApp.Pages.Appointments
 
         private TimeSpan CalculateDuration()
         {
-            var duration = TimeSpan.FromMinutes(Appointment.Services.Sum(s => s.Duration.TotalMinutes));
-            if (duration == TimeSpan.Zero)
+            var duration = TimeSpan.FromMinutes( Appointment.Services.Sum( s => s.Duration.TotalMinutes ) );
+            if ( duration == TimeSpan.Zero )
             {
-                duration = TimeSpan.FromMinutes(15);
+                duration = TimeSpan.FromMinutes( 15 );
             }
             return duration;
         }
 
-        protected async Task GetAvailability_OnClick(DateTime time)
+        protected async Task GetAvailability_OnClick( DateTime time )
         {
             Console.WriteLine( time );
-            var availableTimes = await SchedulingService.GetAvailableAppointmentsTimes(time, Duration);
-            AvailableTimes = availableTimes.OrderBy(a => a.StartTime);
+            var availableTimes = await SchedulingService.GetAvailableAppointmentsTimes( time, Duration );
+            AvailableTimes = availableTimes.OrderBy( a => a.StartTime );
         }
 
-        protected async Task SetStartTime_OnClick(DateTime time)
+        protected async Task SetStartTime_OnClick( DateTime time )
         {
             try
             {
                 StartTime = time;
-                Appointment = await AppointmentService.PatchAppointmentTimes(Id, new UpdateAppointmentTimesDto()
+                Appointment = await AppointmentService.PatchAppointmentTimes( Id, new UpdateAppointmentTimesDto()
                 {
-                    StartTime = (DateTime)StartTime,
-                    EndTime = (DateTime)EndTime
-                });
+                    StartTime = ( DateTime ) StartTime,
+                    EndTime = ( DateTime ) EndTime
+                } );
 
             }
-            catch (Exception)
+            catch ( Exception )
             {
                 throw;
             }
 
-            AppointmentService.RaiseEventOnStartTimeChanged(time);
+            AppointmentService.RaiseEventOnStartTimeChanged( time );
 
             StateHasChanged();
         }
@@ -115,59 +115,59 @@ namespace JricaStudioApp.Pages.Appointments
             AppointmentItemService.OnProductAdded -= AppointmentProdcutsChanged;
         }
 
-        private void AppointmentProdcutsChanged(IEnumerable<AppointmentProductDto> productDtos)
+        private void AppointmentProdcutsChanged( IEnumerable<AppointmentProductDto> productDtos )
         {
             Appointment.Products = productDtos;
 
             StateHasChanged();
         }
-        protected async Task DeleteAppointmentProduct_Click(Guid id)
+        protected async Task DeleteAppointmentProduct_Click( Guid id )
         {
-            var appointmentProduct = await AppointmentItemService.DeleteAppointmentProduct(id);
+            var appointmentProduct = await AppointmentItemService.DeleteAppointmentProduct( id );
 
-            Appointment.Products = await AppointmentItemService.GetAppointmentProducts(Appointment.Id);
-            AppointmentItemService.RaiseEventOnProductsChanged(Appointment.Products);
+            Appointment.Products = await AppointmentItemService.GetAppointmentProducts( Appointment.Id );
+            AppointmentItemService.RaiseEventOnProductsChanged( Appointment.Products );
         }
 
         protected async Task ConfirmAppointmentBooking_Click()
         {
-            if (Appointment != null)
+            if ( Appointment != null )
             {
-                if (Appointment.StartTime != null && Appointment.EndTime != null)
+                if ( Appointment.StartTime != null && Appointment.EndTime != null )
                 {
                     var appointmentTimesDto = new UpdateAppointmentTimesDto()
                     {
-                        StartTime = (DateTime)Appointment.StartTime,
-                        EndTime = (DateTime)Appointment.EndTime
+                        StartTime = ( DateTime ) Appointment.StartTime,
+                        EndTime = ( DateTime ) Appointment.EndTime
                     };
 
-                    Appointment = await AppointmentService.PatchAppointmentTimes(Appointment.Id, appointmentTimesDto);
+                    Appointment = await AppointmentService.PatchAppointmentTimes( Appointment.Id, appointmentTimesDto );
 
-                    if (Appointment.Products.Count() != 0 || Appointment.Services.Count() != 0)
+                    if ( Appointment.Products.Count() != 0 || Appointment.Services.Count() != 0 )
                     {
-                        if (Appointment.Status == AppointmentStatus.WaiverAccepted)
+                        if ( Appointment.Status == AppointmentStatus.WaiverAccepted )
                         {
 
                             Appointment.Status = AppointmentStatus.BookingDateSubmited;
-                            await AppointmentService.PatchAppointmentStatus(Id, new UpdateAppointmentStatusDto()
+                            await AppointmentService.PatchAppointmentStatus( Id, new UpdateAppointmentStatusDto()
                             {
                                 Status = Appointment.Status
-                            });
-                            AppointmentService.RaiseEventOnStatusChanged(Appointment.Status);
+                            } );
+                            AppointmentService.RaiseEventOnStatusChanged( Appointment.Status );
                         }
-                        switch (Appointment.Status)
+                        switch ( Appointment.Status )
                         {
                             case AppointmentStatus.NotFinalized:
-                                NavigationManager.NavigateTo($"/IndemnityForm/{Id}");
+                                NavigationManager.NavigateTo( $"/IndemnityForm/{Id}" );
                                 break;
                             case AppointmentStatus.IndemnityFormSubmited:
-                                NavigationManager.NavigateTo($"/Wavier/{await ManageLocalStorageService.GetLocalUserIdGuid()}");
+                                NavigationManager.NavigateTo( $"/Wavier/{await ManageLocalStorageService.GetLocalUserIdGuid()}" );
                                 break;
                             case AppointmentStatus.WaiverAccepted:
-                                NavigationManager.NavigateTo($"/Appointment/Schedule/{Id}");
+                                NavigationManager.NavigateTo( $"/Appointment/Schedule/{Id}" );
                                 break;
                             case AppointmentStatus.BookingDateSubmited:
-                                NavigationManager.NavigateTo($"/Finalize/{Id}");
+                                NavigationManager.NavigateTo( $"/Finalize/{Id}" );
                                 break;
                             default:
                                 break;
